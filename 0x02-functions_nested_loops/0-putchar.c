b23f1ed4a1417eceab97e27184047d9303b1b790
XimeonLeo <ximeonleo@gmail.com>
2023-03-21 13:43:54 -0700

    First commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *
+  *Return: 0 for success
+  */
+int main(void)
+{
+
+	printf("%s\n", "_putchar");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains ALX projects on nested loops
