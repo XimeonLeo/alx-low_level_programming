ce92b3aec506b5f7950e282ad8d659a13bda8a91
XimeonLeo <ximeonleo@gmail.com>
2023-03-22 13:45:57 -0700

    all about 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,8 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				printf("%d\n", n);
+				printf("%d", n);
+				printf("\n");
 				break;
 			}
 			else
@@ -30,7 +31,8 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				printf("%d\n", n);
+				printf("%d", n);
+				printf("\n");
 				break;
 			}
 			else
