2afff978bd0a2d16c1e64845d3af41853246c254
XimeonLeo <ximeonleo@gmail.com>
2023-03-22 11:21:49 -0700

    9 times table from grade 3

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+  *times_table - Entry point
+  *
+  *Return: void for success
+  */
+
+void times_table(void)
+{
+	int a;
+	int b;
+	int prdt;
+
+	for (a = 0; a < 10; a++)
+	{
+	for (b = 0; b < 10; b++)
+	{
+		prdt = a * b;
+		if (b == 0)
+		{
+			_putchar(prdt + '0');
+		}
+		else if (prdt < 10 && b != 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(prdt + '0');
+		}
+		else if (prdt >= 10)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar((prdt / 10) + '0');
+			_putchar((prdt % 10) + '0');
+		}
+	}
+	_putchar('$');
+	_putchar('\n');
+	}
+}
