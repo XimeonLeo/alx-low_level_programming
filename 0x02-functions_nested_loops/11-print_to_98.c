688fe7a42378c741296420b97c5edbb25c160c3a
XimeonLeo <ximeonleo@gmail.com>
2023-03-22 12:59:41 -0700

    specific about 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+  *print_to_98 - Entrt point
+  *@n: an unknown intrger
+  *Return: 0 for success
+  */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d\n", n);
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+	if (n <= 98)
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d\n", n);
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+}
