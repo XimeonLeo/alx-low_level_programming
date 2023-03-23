cff05ab824b164bb1454bf294c04b32cd1d9f60d
XimeonLeo <ximeonleo@gmail.com>
2023-03-22 18:51:11 -0700

    stop at 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,34 +10,33 @@
 
 void print_to_98(int n)
 {
-	if (n >= 98)
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+		if (n != 98)
+		{
+			printf("%d, ", n);
+		}
+		else if (n == 98)
+		{
+			printf("%d\n", n);
+		}
+		}
+	}
+
+	else if (n >= 98)
 	{
 		for (; n >= 98; n--)
 		{
-			if (n == 98)
-			{
-				printf("%d", n);
-				printf("\n");
-				break;
-			}
-			else
-			{
-				printf("%d, ", n);
-			}
+		if (n != 98)
+		{
+			printf("%d, ", n);
+		}
+		else if (n == 98)
+		{
+			printf("%d\n", n);
+		}
 		}
 	}
-	if (n <= 98)
-		for (; n <= 98; n++)
-		{
-			if (n == 98)
-			{
-				printf("%d", n);
-				printf("\n");
-				break;
-			}
-			else
-			{
-				printf("%d, ", n);
-			}
-		}
 }
