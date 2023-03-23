5e9dc7a858b55cc9aca04aa6ebcab95d4974ea13
XimeonLeo <ximeonleo@gmail.com>
2023-03-22 19:51:36 -0700

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,28 +18,24 @@ void print_times_table(int n)
 	for (b = 0; b <= n; b++)
 	{
 		c = a * b;
-
 		if (b == 0)
 		{
 			_putchar(c + '0');
-		}
-		else if (b != 0 && c < 10)
+		} else if (b != 0 && c < 10)
 		{
 			_putchar(',');
 			_putchar(' ');
 			_putchar(' ');
 			_putchar(' ');
 			_putchar(c + '0');
-		}
-		else if (c >= 10 && c < 100)
+		} else if (c >= 10 && c < 100)
 		{
 			_putchar(',');
 			_putchar(' ');
 			_putchar(' ');
 			_putchar((c / 10) + '0');
 			_putchar((c % 10) + '0');
-		}
-		else if (c >= 100)
+		} else if (c >= 100)
 		{
 			_putchar(',');
 			_putchar(' ');
