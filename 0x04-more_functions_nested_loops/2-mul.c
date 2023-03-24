88be2efd6cf16fe112d0b1d31da1ed6715fa29ae
XimeonLeo <ximeonleo@gmail.com>
2023-03-24 13:38:00 -0700

    multiplication of integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  *mul - To print the multiple of two
+  *digits
+  *@a: first integer to be compited
+  *@b: secomd integer to be computed
+  *Return: 0 for success
+  */
+
+int mul(int a, int b)
+{
+	return (a * b);
+
+}
