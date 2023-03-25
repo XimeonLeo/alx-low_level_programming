6e4a1d0a268388a6fe8b767b75312e705d3b4a20
XimeonLeo <ximeonleo@gmail.com>
2023-03-24 21:12:32 -0700

    0 - 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  *more_numbers - entry point
+  *
+  *return : void for success
+  */
+
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b >= 10)
+			{
+				_putchar((b / 10) + '0');
+			}
+			_putchar((b % 10) + '0');
+		}
+		_putchar('\n');
+	}
+
+}
