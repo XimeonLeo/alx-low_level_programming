b5913f9395889648e9ca1c439ca6ccccf627838c
XimeonLeo <ximeonleo@gmail.com>
2023-03-24 21:46:25 -0700

    drawing a diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  *print_diagonal - entry point
+  *
+  *return: void dor success
+  */
+
+void print_diagonal(int n)
+{
+	int d;
+	int space;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	for (d = 0; d < n; d++)
+	{
+	for (space = 0; space < d; space++)
+	{
+		_putchar(' ');
+	}
+	_putchar('\\');
+	_putchar('\n');
+	}
+}
