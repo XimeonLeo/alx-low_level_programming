a513c97147f09a4f7058aa87e32bd6b14c5527d7
XimeonLeo <ximeonleo@gmail.com>
2023-03-26 17:36:10 -0700

    triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,9 +16,9 @@ void print_triangle(int size)
 	}
 	else
 	{
-		for (i = 0; i < size; i++)
+		for (i = 0; i < (size - 1); i++)
 		{
-			for (j = 0; j < size - i; j++)
+			for (j = 0; j < (size - 1) - i; j++)
 			{
 				_putchar(' ');
 			}
