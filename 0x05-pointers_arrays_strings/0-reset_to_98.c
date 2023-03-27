f2dd1cf99be6aa449572f9b4e6cc367cf8712bc5
XimeonLeo <ximeonleo@gmail.com>
2023-03-27 14:27:14 -0700

    updated header file and added file 0-98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *reset_to_98 - to reset the value
+ *of a pointer to 98
+ *@n: the variable to be updated by
+ *pointer *n
+ *return: void for success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
