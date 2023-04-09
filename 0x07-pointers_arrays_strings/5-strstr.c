baaac0410839a6ea10135a333198bf39acfa6fa5
XimeonLeo <ximeonleo@gmail.com>
2023-04-08 20:06:20 -0700

    check to see for substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *_strstr - finds the first occurrence of a substring in anothet string
+ *
+ *@haystack: main string
+ *@needle: substring
+ *
+ *Return: string haystack from point of occurrence
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int h, n;
+
+	for (h = 0; haystack[h] != '\0'; h++)
+	{
+		if (haystack[h] == needle[0])
+		{
+			for (n = 0; needle[n] != '\0'; n++)
+			{
+				if (haystack[h + n] != needle[n])
+				{
+					break;
+				}
+			}
+			if (needle[n] == '\0')
+			{
+				return (&haystack[h]);
+			}
+		}
+	}
+	return (NULL);
+}
