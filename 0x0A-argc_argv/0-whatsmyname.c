15ed4427ab85fc2ef1421e535814edb02c5f35e9
XimeonLeo <ximeonleo@gmail.com>
2023-04-10 20:09:49 -0700

    Firat commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of this programme
+ *
+ *@argc: argument count of the programe
+ *@argv: argument vector of the progrmme
+ *
+ *Return: 0 for success
+ */
+
+int main(int __attribute((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
