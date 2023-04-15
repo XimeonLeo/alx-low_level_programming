7376c4c446032fd76370dfbdffaafabe4ceb715c
XimeonLeo <ximeonleo@gmail.com>
2023-04-15 18:30:25 +0000

    a multi-dimensional array using malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,44 +1,43 @@
 #include "main.h"
 
 /**
- *alloc_grid - returns a pointer to a two dimensional array
+ *alloc_grid - returns a pointer to a 2 × 2 matrix
+ *of integers.
+ *@width: width of the array.
+ *@height: height of the array.
  *
- *@width: the width of array
- *@height: the height of array
- *
- *Return: a double pointer
+ * Return: pointer of an array of integers
  */
-
 int **alloc_grid(int width, int height)
 {
 	int **arr;
 	int row, column;
 
 	if (width < 1 || height < 1)
-	{
 		return (NULL);
-	}
 
-	arr = malloc(sizeof(int *) * height);
-
+	arr = malloc(height * sizeof(int *));
 	if (arr == NULL)
 	{
+		free(arr);
 		return (NULL);
 	}
 
 	for (row = 0; row < height; row++)
 	{
-		arr[row] = malloc(sizeof(int) * width);
-
+		arr[row] = malloc(width * sizeof(int));
 		if (arr[row] == NULL)
 		{
+			for (row--; row >= 0; row--)
+				free(arr[row]);
+			free(arr);
 			return (NULL);
 		}
-
-		for (column = 0; column < width; column++)
-		{
-			arr[row][column] = 0;
-		}
 	}
+
+	for (row = 0; row < height; row++)
+		for (column = 0; column < width; column++)
+			arr[row][column] = 0;
+
 	return (arr);
 }
