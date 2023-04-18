f1c8666eeacc7c59684b8a8b6bea561eb6bbf7ea
XimeonLeo <ximeonleo@gmail.com>
2023-04-18 18:01:25 +0000

    i got a customer, lets initialize his details

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,16 +19,7 @@ struct dog
 
 };
 
-/**
- * dog_t - user defined data type fot struct dog
- */
-
-typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
 
 #endif /*__DOG_H__*/
