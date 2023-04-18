03f9d97354e75344fb2b561f47721e76378566fc
XimeonLeo <ximeonleo@gmail.com>
2023-04-18 13:59:07 +0000

    my dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef __DOG_H__
+#define __DOG_H__
+
+/**
+ * struct dog - a new data type to classify nake age and owner of a dog
+ *
+ * @name: the name of the required dog
+ * @age: hiw old the dog is
+ * @owner: who the dog belongs too
+ *
+ */
+
+struct dog {
+
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+#endif /*__DOG_H__*/
