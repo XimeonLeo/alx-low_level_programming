d0b1a1f4e9a33466b9fcc340efb0ea52bcaf0822
XimeonLeo <ximeonleo@gmail.com>
2023-04-20 03:00:03 +0000

    fixed indentation

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,7 @@
  * print_name - prints name by derefrencing a functiin pointer
  *
  * @name: a poonter to the name to print
- * @f: a function pointer 
+ * @f: a function pointer
  *
  * Return: nothing
  */
@@ -12,7 +12,7 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
-		{
-			f(name);
-		}
+	{
+		f(name);
+	}
 }
