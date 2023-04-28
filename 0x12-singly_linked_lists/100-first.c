3de4eb362430dc8fc6f16e5a834ab8a537b7df8b
XimeonLeo <ximeonleo@gmail.com>
2023-04-28 09:15:06 +0000

    i could do something before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void __attribute__((constructor)) hare(void);
+void __attribute__((constructor)) hare_and_tortoise(void);
 
 /**
- * hare - Prints a string before executing the main function
+ * hare_and_tortoise - Prints a string before executing the main function
  */
-void hare(void)
+void hare_and_tortoise(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 	       "I bore my house upon my back!\n");
