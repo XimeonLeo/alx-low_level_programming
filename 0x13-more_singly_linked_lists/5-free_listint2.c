1565b30de4ce198c5fa8798733a89eb95ba69005
XimeonLeo <ximeonleo@gmail.com>
2023-05-01 21:09:48 +0000

    freeing then confirming with NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees up the space allocated to listint_t
+ *
+ * @head: the address to the head of the the nodes
+ *
+ * Return: n{thing but free spaces
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tempo;
+
+	if (!(*head))
+	{
+		return;
+	}
+
+	while (*head)
+	{
+		tempo = (*head)->next;
+		free(*head);
+		*head = tempo;
+	}
+
+	*head = NULL;
+
+	
+}
