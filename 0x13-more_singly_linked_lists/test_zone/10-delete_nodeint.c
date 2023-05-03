0b11dba7cb196161bd042e695ce820caa9ace06a
XimeonLeo <ximeonleo@gmail.com>
2023-05-03 15:46:20 +0000

    delete a mode of choice

---
+++ 0x13-more_singly_linked_lists/test_zone/10-delete_nodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - frees the position where a node is no longer needed
+ *
+ * @head: a pointer to the head node
+ * @index: the position where the node is found
+ *
+ * Return: 1 for success, -1 if it fails
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *head_dup = *head, *tmp;
+	unsigned int check = 1;
+
+	if (!(head_dup))
+	{
+		return (-1);
+	}
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(head_dup);
+		return (1);
+	}
+	else
+	{
+		while (check < index)
+		{
+			if (!(head_dup) || !(head_dup->next))
+			{
+				return (-1);
+			}
+			head_dup = head_dup->next;
+			check++;
+		}
+		tmp = head_dup->next;
+		head_dup->next = head_dup->next->next;
+		free(tmp);
+		return (1);
+	}
+
+}
