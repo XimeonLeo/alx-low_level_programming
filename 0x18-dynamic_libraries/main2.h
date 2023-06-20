dea3b7c6d003de9810aa76a0e9ce4ea81b59813f
XimeonLeo <ximeonleo@gmail.com>
2023-06-20 00:48:59 +0000

    added header file

---
+++ 0x18-dynamic_libraries/main2.h
@@ -0,0 +1,12 @@
+#ifndef __MAIN2_H__
+#define __MAIN2_H__
+
+#include <stdio.h>
+
+int add(int dgt1, int dgt2);
+int sub(int dgt1, int dgt2);
+int mul(int dgt1, int dgt2);
+int div(int dgt1, int dgt2);
+int mod(int dgt1, int dgt2);
+
+#endif
