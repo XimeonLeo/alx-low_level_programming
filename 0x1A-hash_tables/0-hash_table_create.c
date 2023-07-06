0e4f6736b623bfabbc5a51d4c1e5cba3619a6bc2
XimeonLeo <ximeonleo@gmail.com>
2023-07-06 13:43:04 +0000

    function that creates a simple hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *myTable = NULL;
+	unsigned long int idx = 0;
+
+	myTable = malloc(sizeof(hash_table_t));
+	if (!myTable)
+		return (NULL);
+
+	myTable->size = size;
+	myTable->array = malloc(sizeof(hash_node_t) * size);
+	if (!myTable->array)
+		return (NULL);
+
+	while (myTable->array[idx])
+	{
+		myTable->array[idx] = NULL;
+		idx++;
+	}
+	return (myTable);
+}
