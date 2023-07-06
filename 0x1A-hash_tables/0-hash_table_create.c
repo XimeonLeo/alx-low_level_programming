92d27e20da6c12f6920fdf2ac301b07b6fe210d5
XimeonLeo <ximeonleo@gmail.com>
2023-07-06 18:07:20 +0000

    properly set all array in the table to n(null)

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -21,7 +21,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!myTable->array)
 		return (NULL);
 
-	while (myTable->array[idx])
+	while (idx < size)
 	{
 		myTable->array[idx] = NULL;
 		idx++;
