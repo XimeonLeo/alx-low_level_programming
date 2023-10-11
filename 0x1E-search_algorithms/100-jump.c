eaf7f6211df7473961303268eccc9c7bd00c587e
XimeonLeo <ximeonleo@gmail.com>
2023-10-11 15:56:52 +0100

    debugging

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -30,7 +30,7 @@ int jump_search(int *array, size_t size, int value)
 
 	if (!array || !size)
 		return (-1);
-	while (array[min(curr, jump - 1)] < value)
+	while (array[min(jump, size - 1)] < value)
 	{
 		printf("Value checked array[%ld] = [%d]\n"
 			, jump, array[jump]);
